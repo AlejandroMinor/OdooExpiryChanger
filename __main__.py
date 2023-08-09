796c191c0beb315ba629cc749bcfc32fc41813a8
Alejandro Minor <aminorj@iingen.unam.mx>
2023-08-08 19:35:43 -0600

    [ADD] Inicio proyecto

---
+++ __main__.py
@@ -0,0 +1,32 @@
+import datetime
+import xmlrpc.client
+
+class DataBaseTools:
+    def __init__(self):
+
+        self.url = "ODOO PROYECT URL"
+        self.db = "DATABASE NAME"
+
+        self.username = 'USERNAME'
+        self.password = 'PASSWORD'
+
+        common = xmlrpc.client.ServerProxy("{}/xmlrpc/2/common".format(self.url))
+        self.models = xmlrpc.client.ServerProxy("{}/xmlrpc/2/object".format(self.url))
+        # Authenticate the user
+        self.uid = common.authenticate(self.db, self.username, self.password, {})
+
+        # Obtiene la fecha actual y le suma 30 días
+        date = datetime.date.today() + datetime.timedelta(days=30)
+        date = date.strftime("%Y-%m-%d")
+
+        target_model = 'ir.config_parameter'
+        # Busca el id del registro con key = 'database.expiration_date'
+        record_id = self.models.execute_kw(self.db, self.uid, self.password, target_model, 'search',[[['key', '=', 'database.expiration_date']]])   
+
+        # actualiza el campo expiration_date de la tabla ir.config_parameter
+        update = self.models.execute_kw(self.db, self.uid, self.password, target_model, 'write',[record_id, {'value': date}])
+
+        if update:
+            print(f"Se actualizó el registro con la fecha {date}")
+        else:
+            print("No se actualizó el registro")
